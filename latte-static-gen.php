b437eb9b2879a6f61bf6138e8790c33bfcf76dc8
Vit Peprnicek <vit.peprnicek@nwt.cz>
2022-07-05 15:08:22 +0200

    Fix loading files, showing errors

--- latte-static-gen.php
+++ latte-static-gen.php
@@ -1,41 +1,38 @@
 #!/usr/bin/env php
 <?php declare(strict_types=1);
 
-use Murdej\LatteStaticGenerator\TaskConfig;
 use Murdej\LatteStaticGenerator\Generator;
 use Murdej\LatteStaticGenerator\ProjectParser;
 
-require $_composer_autoload_path ?? __DIR__ . '/vendor/autoload.php';
-// require __DIR__ . '/App/Generator.php';
-// require __DIR__ . '/App/TaskConfig.php';
-// require __DIR__ . '/App/CliParserFactory.php';
-// require __DIR__ . '/App/ProjectParser.php';
-// require __DIR__ . '/App/Project.php';
-// require __DIR__ . '/Plugins/IPlugin.php';
-// require __DIR__ . '/Plugins/PluginEvent.php';
-// require __DIR__ . '/Plugins/BasePlugin.php';
-// require __DIR__ . '/Plugins/MarkdownPandoc.php';
-
-// $flags = (new CliParserFactory($_SERVER['argv']))->createCliParser();
-
-// try {
-	// if ($flags->parse()) {
-		$src = $_SERVER['argv'][1]; // $flags->getArg(0);
-		$project = (new ProjectParser())->parseNeonFile($src);
-
-		$generator = new Generator(__DIR__ . '/temp');
-		$generator->processProject($project);
-	// } else $flags->displayHelp();
-/* }
-catch(Exception $e)
+function showError(string $message, Exception|null $ex = null, bool $fullError = false)
 {
-	$flags->displayHelp();
-} */
+	echo $message . "\n";
+	if ($ex) echo ($fullError ? $ex->__toString() : $ex->getMessage()) . "\n";
+	exit(1);
+}
 
-/* $task = new TaskConfig();
-$task->sourceFile = './samples/sources/homepage.latte';
-$task->destination = './samples/dist/';
-$task->data['name'] = 'Murdej';
+if (isset($_composer_autoload_path)) require $_composer_autoload_path;
+else if (file_exists($fn = __DIR__ . '/../../autoload.php')) require $fn;
+else if (file_exists($fn = __DIR__ . '/vendor/autoload.php')) require $fn;
+else {
+	showError("Install composer first https://getcomposer.org/download/.");
+}
 
-$generator = new Generator('./temp');
-$generator->processTask($task); */
+
+try {
+	$src = $_SERVER['argv'][1]; // $flags->getArg(0);
+	$project = (new ProjectParser())->parseNeonFile($src);
+	$generator = new Generator(__DIR__ . '/temp');
+	$generator->processProject($project);
+} 
+catch(Nette\Neon\Exception $ex) 
+{
+	showError("Error in configration", $ex);
+}
+catch(Latte\CompileException $ex)
+{
+	showError("Latte compilation error", $ex);
+}
+catch(Throwable $ex) {
+	showError("Unexcepted error", $ex, true);
+}
